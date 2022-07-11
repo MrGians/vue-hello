241ac0294e31e0a7438066ae4e4e24482933f6b4
MrGians <gianlumura97@gmail.com>
2022-07-11 13:18:17 +0200

    Milestone 1 Done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("Vue OK", Vue);
+
+const root = new Vue (
+  {
+    name: "My First Vue",
+    el: "#hello-vue",
+    data: {
+      firstName: "Gianluca",
+      lastName: "Mura",
+    }
+  }
+);
